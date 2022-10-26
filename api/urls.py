c02e7d43b628da1a613807518c359eb293ee6f3a
redgrin <edgar.elps@gmail.com>
2022-10-26 12:58:08 -0500

    serializer del registro de usuario

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from rest_framework import permissions
+from rest_framework.routers import DefaultRouter
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
+
+from .views import RegisterView
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -16,7 +20,10 @@ schema_view = get_schema_view(
    permission_classes=[permissions.AllowAny],
 )
 
+router = DefaultRouter()
+
 urlpatterns = [
-   path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('auth/register', RegisterView.as_view(), name='register'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from api.models import User
+from api.serializers import UserRegisterSerializer
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserRegisterSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
